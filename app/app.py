6a86b2ab7d83cbc360df230e65d39f471cb092f2
Silvio Miranda <silviom727@gmail.com>
2021-12-07 19:22:57 -0600

    uso de templates ninja2

--- app/app.py
+++ app/app.py
@@ -1,11 +1,18 @@
-from flask import Flask
-import flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hola mundo'
+    cursos = ['Aritmetica', 'Algebra', 'Geometria', 'Calculo']
+    data = {
+    
+        'titulo' : 'Index',
+        'Bienvenida' : 'Saludos',
+        'cursos' : cursos ,
+        'Numero de cursos' : len(cursos)
+    }
+    return render_template('index.html', data = data)
 
 if __name__ == '__main__':
     app.run()

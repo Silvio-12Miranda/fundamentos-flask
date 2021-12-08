956e95bf11b3acfbe6efc156ab001f4155bc4aba
Silvio Miranda <silviom727@gmail.com>
2021-12-08 03:06:58 -0600

    uso de query string

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 
 app = Flask(__name__)
 
@@ -14,5 +14,22 @@ def index():
     }
     return render_template('index.html', data = data)
 
+@app.route('/contacto/<nombre>/<int:edad>')
+def contacto(nombre, edad):
+    data = {
+        'titulo' : 'contacto',
+        'nombre' : nombre,
+        'edad' : edad
+    }
+    return render_template('contacto.html', data = data)
+
+def query_string():
+    print(request)
+    print(request.args.get('param_one'))
+    print(request.args.get('param_two'))
+    return 'OK'    
+
+
 if __name__ == '__main__':
+    app.add_url_rule('/query_string', view_func= query_string)
     app.run()
